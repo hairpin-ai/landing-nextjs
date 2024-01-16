2d015c8c8f297291e548ce24d473bfa6f9376b42
hamigg <h@7thw.com>
2024-01-16 15:40:16 -0500

    init

---
+++ next.config.js
@@ -0,0 +1,20 @@
+const withBuilderDevTools = require('@builder.io/dev-tools/next')();
+
+module.exports = withBuilderDevTools({
+  trailingSlash: true,
+  modularizeImports: {
+    '@mui/material': {
+      transform: '@mui/material/{{member}}',
+    },
+    '@mui/lab': {
+      transform: '@mui/lab/{{member}}',
+    },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+});
